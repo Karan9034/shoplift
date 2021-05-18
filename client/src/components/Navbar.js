bb850e2ad6ded3fca9dd7466c3ebd1f43f82a637
Karan9034 <karan.agr9034@gmail.com>
2021-05-18 23:51:35 +0530

    Home page under construction

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
-import { Toolbar } from "@material-ui/core";
+import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
+import Menu from "@material-ui/core/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import InputBase from "@material-ui/core/InputBase";
 
 const Navbar = () => {
     return (
@@ -21,6 +25,10 @@ const Navbar = () => {
                 <Typography variant="h6" noWrap>
                     Shoplift
                 </Typography>
+
+                <IconButton edge="end" color="primary">
+                    <AccountCircle />
+                </IconButton>
             </Toolbar>
         </AppBar>
     );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,51 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import Grid from "@material-ui/core/Grid";
 
 const Home = () => {
-    return <h1>Home</h1>;
+    let lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    return (
+        <div>
+            <Grid container spacing={3}>
+                {lst.map((i) => (
+                    <Grid key={i} item md={4} sm={6} xs={12}>
+                        <Card>
+                            <CardActionArea>
+                                {/* <CardMedia
+                                    image="/static/images/cards/contemplative-reptile.jpg"
+                                    title="Contemplative Reptile"
+                                /> */}
+                                <CardContent>
+                                    <Typography
+                                        gutterBottom
+                                        variant="h5"
+                                        component="h2"
+                                    >
+                                        Lizard
+                                    </Typography>
+                                    <Typography
+                                        variant="body2"
+                                        color="textSecondary"
+                                        component="p"
+                                    >
+                                        Lizards are a widespread group of
+                                        squamate reptiles, with over 6,000
+                                        species, ranging across all continents
+                                        except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    );
 };
 
 export default Home;
